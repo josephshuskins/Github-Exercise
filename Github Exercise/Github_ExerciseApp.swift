c80dda4af6196ffd0939d5c9aa5887e70574145d
Scott Huskins <Scott_huskins@yahoo.com>
2022-11-15 05:09:38 -0500

    Initial Commit

---
+++ Github Exercise/Github_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_ExerciseApp.swift
+//  Github Exercise
+//
+//  Created by unix on 11/15/22.
+//
+
+import SwiftUI
+
+@main
+struct Github_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
